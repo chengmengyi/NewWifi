619e2666bcceb8ddfec866c9ee64f36876447f55
chengmengyi <243237049@qq.com>
2023-01-31 14:03:43 +0800

    1

--- app/src/main/java/com/demo/newwifi/App.kt
+++ app/src/main/java/com/demo/newwifi/App.kt
@@ -2,12 +2,15 @@ package com.demo.newwifi
 
 import android.app.Application
 import com.demo.newwifi.uti.ActivityCallback
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.initialize
 import com.tencent.mmkv.MMKV
 import org.litepal.LitePal
 
 class App:Application() {
     override fun onCreate() {
         super.onCreate()
+        Firebase.initialize(this)
         MMKV.initialize(this)
         LitePal.initialize(this)
         ActivityCallback.register(this)
